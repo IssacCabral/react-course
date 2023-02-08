82b73aeb846236c900eeebdc6b7f48a529eb463c
Issac Cabral <issaccabrall@gmail.com>
2023-02-08 15:10:35 -0300

    refactor: add mocked values

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,34 @@ import NewExpense from "./components/NewExpense";
 const INITIAL_EXPENSES = [
   {
     id: "e1",
-    title: "Lavada no carro",
-    amount: 10.5,
-    date: new Date(2023, 3, 6),
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
   },
-  {
-    id: "e2",
-    title: "Registro do fogão",
-    amount: 60,
-    date: new Date(2022, 2, 6),
-  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: "e3",
-    title: "Bucha de canhão",
-    amount: 130,
-    date: new Date(2023, 2, 6),
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+  {
+    id: "e5",
+    title: "Car Wash",
+    amount: 20.56,
+    date: new Date(2023, 2, 28),
+  },
+  {
+    id: "e6",
+    title: "Mouse",
+    amount: 45,
+    date: new Date(2023, 5, 12),
   },
 ];
 
