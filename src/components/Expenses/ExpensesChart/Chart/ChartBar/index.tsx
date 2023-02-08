84fe5066e41a1ef1768d990b8baea4aedcdcb526
Issac Cabral <issaccabrall@gmail.com>
2023-02-08 15:06:31 -0300

    fix: resolve Chart styles

--- src/components/Expenses/ExpensesChart/Chart/ChartBar/index.tsx
+++ src/components/Expenses/ExpensesChart/Chart/ChartBar/index.tsx
@@ -9,8 +9,8 @@ interface ChartBarProps {
 export const ChartBar = (props: ChartBarProps) => {
   let barFillHeight = "0%";
 
-  if (props.value > 0) {
-    barFillHeight = Math.round((props.value / props.maxValue) * 100).toString();
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
 
   return (
@@ -18,10 +18,10 @@ export const ChartBar = (props: ChartBarProps) => {
       <div className="chart-bar__inner">
         <div
           className="chart-bar__fill"
-          style={{ height: barFillHeight, backgroundColor: "red" }}
+          style={{ height: barFillHeight }}
         ></div>
       </div>
-      <div className="chart-bar__label"></div>
+      <div className="chart-bar__label">{props.label}</div>
     </div>
   );
 };
